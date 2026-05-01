5b486da5a9512b85955421c42f5d4f2e66f19273
Natã Vitor Vaguel Reis <59771238+NVPanda@users.noreply.github.com>
2026-05-01 13:35:29 -0300

    Refactor Navigation component for improved styling and functionality

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,70 +1,88 @@
-import React from 'react'
-import styled from 'styled-components'
-import Button from './Button'
-import Logo from './Logo'
-
-
+import React from 'react';
+import styled from 'styled-components';
+import Button from './Button';
+import Logo from './Logo';
 
 const Section = styled.section`
-width: 100vw;
-background-color: ${props => props.theme.body};
-`
+  width: 100%; /* Alterado de 100vw para 100% para evitar scroll horizontal */
+  background-color: ${props => props.theme.body};
+`;
 
 const NavBar = styled.nav`
-display: flex;
-justify-content: space-between;
-align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-width: 80%;
-height: ${props => props.theme.navHeight};
-margin: 0 auto;
-`
+  width: 85%;
+  height: ${props => props.theme.navHeight || '5rem'};
+  margin: 0 auto;
+
+  @media (max-width: 640px) {
+    width: 90%;
+  }
+`;
+
 const Menu = styled.ul`
-display: flex;
-justify-content: space-between;
-align-items: center;
-list-style: none;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
 
-const MenuItem = styled.il`
-margin: 0 1rem;
-color: ${props => props.theme.text};
-cursor: pointer;
+  @media (max-width: 768px) {
+    display: none; /* Oculta o menu em dispositivos móveis (ideal implementar um Menu Hambúrguer depois) */
+  }
+`;
 
-&::after {
-  content: " ";
-  display: block;
-  width: 0%;
-  height: 2px;
-  background: ${props => props.theme.text};
-  transition: width 0.3s ease;
+const MenuItem = styled.li` /* Corrigido de il para li */
+  margin: 0 1rem;
+  color: ${props => props.theme.text};
+  cursor: pointer;
+  font-weight: 500;
 
-}
+  &::after {
+    content: " ";
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${props => props.theme.text};
+    transition: width 0.3s ease;
+  }
 
-&hover::after{
-  width: 100%
-}
-`
+  &:hover::after { /* Corrigido de &hover para &:hover */
+    width: 100%;
+  }
+`;
 
 const Navigation = () => {
-  return (
 
-    <Section>
+  const scrollTo = (id) => {
+    let element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+      });
+    }
+  };
+
+  return (
+    <Section id="navigation">
       <NavBar>
         <Logo />
         <Menu>
-          <MenuItem>Home</MenuItem>
-          <MenuItem>Roadmap</MenuItem>
-          <MenuItem>Showcase</MenuItem>
-          <MenuItem>Team</MenuItem>
-          <MenuItem>Faq</MenuItem>
-          <MenuItem>About</MenuItem>
+          <MenuItem onClick={() => scrollTo('home')}>Home</MenuItem>
+          <MenuItem onClick={() => scrollTo('roadmap')}>Roadmap</MenuItem>
+          <MenuItem onClick={() => scrollTo('showcase')}>Showcase</MenuItem>
+          <MenuItem onClick={() => scrollTo('team')}>Team</MenuItem>
+          <MenuItem onClick={() => scrollTo('faq')}>Faq</MenuItem>
         </Menu>
-        <Button />
         
+        {/* Passando props para o seu componente Button que ajustamos */}
+        <Button text="Conectar" link="https://discord.gg/seu-convite" />
       </NavBar>
     </Section>
-  )
-}
+  );
+};
 
 export default Navigation;
