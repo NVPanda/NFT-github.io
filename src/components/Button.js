7e28fc827c9e6c6dc0f30574e6d6bb01b3149465
Natã Vitor Vaguel Reis <59771238+NVPanda@users.noreply.github.com>
2026-05-01 13:30:32 -0300

    Refactor Button component and styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,54 +1,52 @@
-import React from 'react'
-import Styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-const Btn = Styled.button`
+// 1. Corrigido para styled.button e adicionado position: relative
+const Btn = styled.button`
+  display: inline-block;
+  background-color: ${props => props.theme.text};
+  color: ${props => props.theme.body};
+  outline: none;
+  border: none;
+  font-size: ${props => props.theme.fontsm || '0.875rem'};
+  padding: 0.9rem 2.3rem; /* Corrigido de 2.3em para rem para consistência */
+  border-radius: 50px;
+  cursor: pointer;
+  transition: all 0.2s ease;
+  position: relative; /* Essencial para o posicionamento do pseudo-elemento :after */
 
-display: inline-block;
-background-color: ${props => props.theme.text};
-color: ${props => props.theme.body};
-outline: none;
-border: none;
-font-size: ${props => props.theme.fontsm};
-padding: 0.9rem 2.3em;
-border-radius: 50px;
-cursor: pointer;
-transition: all 0.2s ease;
+  &:hover {
+    transform: scale(0.9);
+  }
 
-&:hover{
-  transform: scale(0.9);
-}
+  &::after {
+    content: ' ';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) scale(0);
+    border: 2px solid ${props => props.theme.text};
+    width: 100%;
+    height: 100%;
+    border-radius: 50px;
+    transition: all 0.2s ease;
+  }
 
-&:after{
-  content: ' ';
-   position: absolute;
-   top: 50%;
-   left: 50%;
-   transform: translate(-50%, -50%) scale(0);
-   border: 2px solid ${props => props.theme.text};
-   width: 100%;
-   height: 100%;
-   border-radius: 50px;
-   transition: all 0.2s ease;
-}
+  &:hover::after {
+    transform: translate(-50%, -50%) scale(1);
+    padding: 0.3rem; /* Corrigido de 0,3rem para 0.3rem */
+  }
+`;
 
-&:hover::after{
-  transform: scale(1);
-  padding: 0,3rem;
-}
-`
+// 2. Componente Reutilizável
+const Button = ({ text, link }) => {
+  return (
+    <a href={link} aria-label={text} target="_blank" rel="noreferrer">
+      <Btn>
+        {text}
+      </Btn>
+    </a>
+  );
+};
 
-
-
-const Button = ({text, link}) => {
-  return
-    <Btn>
-<a href={link} aria-label={text} target="_blank" rel="noreferrer">
-{text}
-    <a href={'https://www.binance.com/en/nft/my-nfts/disparattechno-0b912ff682faf6da0863d45e1dbd9500'} aria-label={text} target="_blank" rel="noreferrer" >
-<Btn>{'Discord Coding Community College'}</Btn>
-</a>
-    </Btn>
-  )
-}
-
-export default Button
+export default Button;
